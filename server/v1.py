93b6aa209075334353e84ac9d0d6ff3221c29738
tezansahu <tezansahu@gmail.com>
2021-04-23 13:51:30 +0530

    integrate open redirect detection into server & dashboard

--- server/v1.py
+++ server/v1.py
@@ -1,12 +1,15 @@
+import re
 from fastapi import APIRouter
 from pydantic import BaseModel
 from phishing_detection import detector
 from tls_cert_detection import cert_checker
+from open_redirect_detection import or_detector
 
 router = APIRouter()
 
 tls_cc = cert_checker.CertChecker()
 pwd = detector.PhishingWebsiteDetector()
+or_d = or_detector.OpenRedirectsDetector()
 
 class ScanOptions(BaseModel):
     tls_cert: bool = True
@@ -22,7 +25,10 @@ def get_v1_root():
 
 @router.post("/scan/")
 def vulnerablity_scan(url: str, options: ScanOptions):
+    if not re.match(r"^https?", url):
+        url = "https://" + url
     response = {"url": url}
+    
     if options.tls_cert:
         tls_res = tls_cc.checkCertChain(url)
         response["tls_cert"] = tls_res
@@ -32,6 +38,7 @@ def vulnerablity_scan(url: str, options: ScanOptions):
         pwd_res = pwd.detect_phishing(url)
         response["phishing"] = pwd_res
     if options.open_redirect:
-        pass
+        ord_res = or_d.detect_or(url)
+        response["open_redir"] = ord_res
     
     return response
