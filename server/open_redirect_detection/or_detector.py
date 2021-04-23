d2a4369497daf5681e814a766c848d1c544eea08
shreyaladdha <2000sladdha@gmail.com>
2021-04-23 11:38:11 +0530

    OR detector updated

--- server/open_redirect_detection/or_detector.py
+++ server/open_redirect_detection/or_detector.py
@@ -10,59 +10,58 @@ class OpenRedirectsDetector:
     
     def detect_or(self, url):
         self.url = url
-        result = self.request()
+        result = self.check()
         return result
 
-    def request(self):
-        request = requests.Session()
-        try:
-            page = request.get(self.url, allow_redirects=False, timeout=10, verify=False, params='')
-        except requests.exceptions.Timeout:
-            return {"result": "TimeOut", "details": {"url": self.url}}
-        except requests.exceptions.ConnectionError:
-             return {"result": "Connection Error", "details": {"url": self.url}}
-
-        result = self.check(page,page.request.url)
-        return result
-
-    def check(self, PageVar,FinalUrl):
-        RedirectCodes = [i for i in range(300,311,1)]
+    def check(self):
         values = ['url', 'rurl', 'u','next', 'link', 'lnk', 'go', 'target', 'dest', 'destination', 'redir', 
         'redirect_uri', 'redirect_url', 'redirect', 'view', 'loginto', 'image_url', 'return', 'returnTo', 'return_to',
         'continue', 'return_path', 'path']
 
-        parsed = urlparse.urlparse(FinalUrl)
+        parsed = urlparse.urlparse(self.url)
         params = urlparse.parse_qsl(parsed.query)
 
-        if PageVar.status_code in RedirectCodes:
-            details = {}
-            for x,y in params:
-                if(x in values):
-                    details = {"parameter": x, "redirect_url": y }
-                    break
+        details = {}
+        for x,y in params:
+            if(x in values):
+                details = {"parameter": x, "redirect_url": y }
+                break
 
-            if(details):
-                return {"result": "Header Based Redirection", "details": details}
+        if(details):
+            return {"result": "Header Based Redirection", "details": details}
 
-            elif('/r/' in FinalUrl):
-                details = {"parameter": 'r'}
-                return {"result": "Header Based Redirection", "details": details}
-            else:
-                return {"result": "Open Redirect Vulnerable", "details": {}}
-
-        elif PageVar.status_code==404:
-            return {"result": "Error [404]", "details": {}}
-        elif PageVar.status_code==403:
-            return {"result": "Error [403]", "details": {}}
-        elif PageVar.status_code==400:
-            return {"result": "Error [400]", "details": {}}
-
+        # elif('/r/' in FinalUrl):
+        #     details = {"parameter": 'r'}
+        #     return {"result": "Header Based Redirection", "details": details}
         else:
+            payload = "google.co.in"
+            query = ["url", "redirect_url"]
+            RedirectCodes = [i for i in range(300,311,1)]
+            for x in query:
+                url = self.url+"?"+x+"="+payload
+                print(url)
+                request = requests.Session()
+                try:
+                    page = request.get(url, allow_redirects=False, timeout=10, verify=False, params='')
+                    if page.status_code in RedirectCodes:
+                        return {"result": "Header Based Redirection", "details": {"parameter": x}}
+
+                    elif page.status_code==404:
+                        return {"result": "Error [404]", "details": {}}
+                    elif page.status_code==403:
+                        return {"result": "Error [403]", "details": {}}
+                    elif page.status_code==400:
+                        return {"result": "Error [400]", "details": {}}
+                except requests.exceptions.Timeout:
+                    return {"result": "TimeOut", "details": {"url": self.url}}
+                except requests.exceptions.ConnectionError:
+                    return {"result": "Connection Error", "details": {"url": self.url}}
             return {"result": "Not Vulnerable", "details": {}}
+
 
 def main():
     o = OpenRedirectsDetector()
-    print(o.detect_or("https://medium.com/r/?url=www.google.com"))
+    print(o.detect_or("https://bugslayers-cs416-open-redirect.herokuapp.com/"))
 
 if __name__ == "__main__":
     main()
